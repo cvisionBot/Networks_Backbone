9a67eb686c43b48150199bda5cf86f71f1f08b7b
yooninsig <cvisionyoon@gmailc.om>
2022-01-02 18:45:27 +0900

    ResNet Networks

--- models/ResNet/README.md
+++ models/ResNet/README.md
@@ -17,7 +17,7 @@
 - Deep Networks low/mid/high 단계의 Feature 통합한다. 그리고 Multi-Layer의 end-to-end의 Classifier는 널리 퍼지게 하며, 각 Feature의 "Level"은 Layer 깊게 쌓으면서 보강될 수 있다.
 - 따라서 Depth는 중요하며, 결과를 이끈다.
 
-![1.JPG](Deep%20Residual%20Learning%20for%20Image%20Recognition%207354b7eeff774dcf9f7f9452d889cf1a/1.jpg)
+![Figure1](./src/1.jpg)
 
 - Depth의 중요성에 입각하여, "Network에 Layer 추가하는 방향이 더 좋은가? 에 대한 질문이 따라오게 된다.
 - 이 질문에 답하기에 가장 큰 장애물은 `Vanishing/Exploding Gradients` 문제이다.
@@ -27,7 +27,7 @@
 
 $F(x) := H(x) - x$
 
-![2.JPG](Deep%20Residual%20Learning%20for%20Image%20Recognition%207354b7eeff774dcf9f7f9452d889cf1a/2.jpg)
+![Figure2](./src/2.jpg)
 
 - 해당 공식의 경우 `Shortcut Conntection` 이해할 수 있다. 이는 하나 또는 여러 Layer 건너 뛴다.
 - 저자들은 `Identiy Mapping` 이용하여 수행하였고, 이 output은 쌓여진 Layer에 더해진다. `Identiy Shortcut Connection` 추가 Parameter가 필요하지 않고, Computation 또한 복잡하지 않다.
@@ -62,9 +62,9 @@ $F(x) := H(x) - x$
 
 **Network Architectures**
 
-![4.JPG](Deep%20Residual%20Learning%20for%20Image%20Recognition%207354b7eeff774dcf9f7f9452d889cf1a/4.jpg)
+![Table1](./src/4.jpg)
 
-![3.JPG](Deep%20Residual%20Learning%20for%20Image%20Recognition%207354b7eeff774dcf9f7f9452d889cf1a/3.jpg)
+![Figure3](./src/3.jpg)
 
 - ResNet (Residual Network) 구성하면서 2가지 Option 고려한다.
 1. Shortcut은 zero padded 하여 차원을 증가 시키며 수행된다. (Parameter 증가 없음)
@@ -81,7 +81,7 @@ $F(x) := H(x) - x$
 
 **Deeper Bottlenect Architectures**
 
-![5.JPG](Deep%20Residual%20Learning%20for%20Image%20Recognition%207354b7eeff774dcf9f7f9452d889cf1a/5.jpg)
+![Figure5](./src/5.jpg)
 
 - Training-Time 인하여 Building Block → `BottleNeck` 디자인하였다.
 - 해당 `1 x 1 Convolution` 차원을 줄이고 늘리는 기능만 담당한다. 일반 3 x 3 연산과 동일한 Complexity 갖는다.
---
+++ models/layers/blocks.py
@@ -0,0 +1,44 @@
+from ..layers.convolution import Conv2dBnAct, Conv2dBn
+
+import torch
+from torch import nn
+
+class Residual_Block(nn.Module):
+    def __init__(self, in_channels, kernel_size, out_channels, stride=1):
+        super(Residual_Block, self).__init__()
+        self.out_channels = out_channels
+        self.res_conv1 = Conv2dBnAct(in_channels=in_channels, out_channels=in_channels, kernel_size=1, stride=1)
+        self.res_conv2 = Conv2dBnAct(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size, stride=1)
+        self.res_conv3 = Conv2dBn(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride)
+        self.identity = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride)
+  
+    def forward(self, input):
+        output = self.res_conv1(input)
+        output = self.res_conv2(output)
+        output = self.res_conv3(output)
+        if input.size() != output.size():
+            input = self.identity(input)
+        output = input + output
+        return output
+    
+    def get_channel(self):
+        return self.out_channels
+    
+class Residual_LiteBlock(nn.Module):
+    def __init__(self, in_channels, kernel_size, out_channels, stride=1):
+        super(Residual_LiteBlock, self).__init__()
+        self.out_channels = out_channels
+        self.res_conv1 = Conv2dBnAct(in_channels=in_channels, out_channels=in_channels, kernel_size=kernel_size, stride=1)
+        self.res_conv2 = Conv2dBn(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride)
+        self.identity = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=1, stride=stride)
+        
+    def forward(self, input):
+        output = self.res_conv1(input)
+        output = self.res_conv2(output)
+        if input.size() != output.size():
+            input = self.identity(input)
+        output = input + output
+        return output
+
+    def get_channel(self):
+        return self.out_channels
